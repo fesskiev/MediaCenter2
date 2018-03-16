0f56d4cfe76ed4a0b8c42b5aeeb3081998624da0
M.Konchakovskiy <fesskiev@gmail.com>
2018-03-16 15:52:33 +0200

    fix styles

--- mobile/src/main/java/com/fesskiev/mediacenter/ui/media/video/details/VideoFilesActivity.kt
+++ mobile/src/main/java/com/fesskiev/mediacenter/ui/media/video/details/VideoFilesActivity.kt
@@ -1,11 +1,12 @@
 package com.fesskiev.mediacenter.ui.media.video.details
 
 import android.os.Bundle
+import com.fesskiev.mediacenter.R
 import com.fesskiev.mediacenter.domain.entity.media.VideoFolder
 import com.fesskiev.mediacenter.utils.Constants.Companion.EXTRA_VIDEO_FOLDER
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_video_files.*
 import javax.inject.Inject
-
 
 class VideoFilesActivity : DaggerAppCompatActivity(), VideoFilesContract.View {
 
@@ -15,8 +16,20 @@ class VideoFilesActivity : DaggerAppCompatActivity(), VideoFilesContract.View {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_files)
+        setupToolbar()
+        val videoFolder: VideoFolder = intent.extras.getParcelable(EXTRA_VIDEO_FOLDER)
+    }
 
-        val videoFolder : VideoFolder = intent.extras.getParcelable(EXTRA_VIDEO_FOLDER)
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 
     override fun showProgressBar() {
